8235fcf1972858eb3ab4adf0771d0afc67d2f3f2
barnettm <barnett717@gmail.com>
2017-10-17 17:29:25 -0700

    started index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    return Math.floor(Math.random()*1000)+1
+}
+
 ReactDOM.render(
     <h1 className="container">React Prototypes!</h1>,
     document.getElementById('root')
