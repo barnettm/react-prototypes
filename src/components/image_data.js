08da3879890d8472f7cfc39185d7514b6222333f
barnettm <barnett717@gmail.com>
2017-10-26 13:56:34 -0700

    completed stling

---
+++ src/components/image_data.js
@@ -0,0 +1,75 @@
+import dog from './images/dog.jpeg';
+import dog1 from './images/dog1.jpeg';
+import dog2 from './images/dog2.jpg';
+import dog3 from './images/dog3.jpg';
+import dog4 from './images/dog4.jpeg';
+import dog5 from './images/dog5.jpeg';
+import dog6 from './images/dog6.jpeg';
+import dog7 from './images/dog7.jpg';
+
+
+
+export default [
+    {
+        src: dog,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: dog1,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: dog2,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: dog3,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: dog4,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: dog5,
+        style: {
+            right: '25%',
+            top: '48%',
+            transform: 'rotate(16deg)'
+        }
+    },
+    {
+        src: dog6,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(-5%)'
+        }
+    },
+    {
+        src: dog7,
+        style: {
+            left: '25%',
+            top: '52%',
+            transform: 'rotate(-16deg)'
+        }
+    }
+
+];
---
+++ src/components/scrapbook.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './scrapbook.css';
+
+import imageData from './image_data';
+import ScrapbookImage from './scrapbook_images';
+
+function Scrapbook(){
+    const images = imageData.map((img,index) => {
+        return <ScrapbookImage key={index} about={img}/>
+    });
+    return(
+        <div className="scrapbook-container">
+            {images}
+        </div>
+    )
+}
+
+export default Scrapbook;
