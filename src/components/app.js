9536e9fa6635b9db4b8d0da012ab86d6c581b2fd
barnettm <barnett717@gmail.com>
2017-10-31 14:46:58 -0700

    completed basic redux clock

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import Clock from './clock';
+
+
+export default () => {
+    return(
+        <div className="container">
+            <h1 className="text-center">Hey! Wanna know the time?</h1>
+            <Clock />
+        </div>
+    )
+}
 
 
